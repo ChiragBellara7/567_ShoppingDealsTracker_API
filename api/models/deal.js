2cc1aff22af9b87ddc56d84f1b67410817f2f716
harshitha-y <97040380+harshitha-y@users.noreply.github.com>
2024-02-17 12:41:18 -0500

    Update deal.js
    Upgrades to the initial database model, added a few extra fields

--- api/models/deal.js
+++ api/models/deal.js
@@ -2,14 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // This is the schema model for the mongoDB and the expected values to be saved in a JSON'esq format.
 const dealSchema = mongoose.Schema({
-    title: String,
-    body: String,
+    ID: {
+        type: Schema.Types.ObjectId,
+        auto: true
+    },
+    name: String,
     price: Number,
-    image: String,
+    category: String,
+    shortDesc: String,
+    longDesc: String,
+    couponCode: String,
     createdAt: {
         type: Date,
         default: Date.now,
     },
+    expiryDate: Date,
+    image: String
 });
 
 module.exports = mongoose.model("Deal", dealSchema);
